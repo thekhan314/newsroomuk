bb5a1a4ec9e94305584285160aea53a9f9f3df02
Umar khan <umarkhan314@outlook.com>
2021-03-07 04:25:49 -0600

    beam go brr

--- beam_example.py
+++ beam_example.py
@@ -1,18 +1,24 @@
+from datetime import datetime
+print(datetime.now())
+
 from typing import List, Optional
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem.porter import *
 import string
 import re
 import apache_beam as beam
+
 import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
 from nltk.stem.porter import *
 import warnings
 warnings.filterwarnings('ignore') 
+import pyarrow
 
-from datetime import datetime
-print(datetime.now())
+stemmer = PorterStemmer()
+
+
 
 df_hate_words = pd.read_csv('data/raw_other/hateful_ngrams.csv')
 df_hate_words.set_index('ngram',drop=True,inplace=True)
@@ -20,52 +26,65 @@ dict_hateweights = df_hate_words['prophate'].to_dict()
 hate_list = list(df_hate_words.index)
 
 
-
-stemmer = PorterStemmer()
-
 def tokenize(tweet):
     """Removes punctuation & excess whitespace, sets to lowercase,
     and stems tweets. Returns a list of stemmed tokens."""
+    #tweet = " ".join(re.split("[^a-zA-Z]*", tweet.lower())).strip()
     
-    tweet = " ".join(re.split("[^a-zA-Z]*", tweet.lower())).strip()
-    #tokens = re.split("[^a-zA-Z]*", tweet.lower())
+    tokens = re.split("[^a-zA-Z]*", tweet.lower())
     tokens = [stemmer.stem(t) for t in tweet.split()]
     return tokens
+    
+def ngrams(tokens,n):
+    ngrams = zip(*[tokens[i:] for i in range(n)])
+    list_gram = [" ".join(ngram) for ngram in ngrams]
+    return list_gram
 
-unlab_cvect = CountVectorizer(
-    ngram_range=(1,4),
-    vocabulary = hate_list,
-    tokenizer = tokenize
-)
-def tally_counts_doc(row):
-    row2 = row[row > 0]
-    score = 0
-    hits = {}
-    for index,val in row2.items():
-        hits[index]=val
-        hit = dict_hateweights[index] * val
-        score += hit 
-    row['hate_score'] = score
-    row['hate_hits'] = hits
 
-    return row
 
 class WordExtractingDoFn(beam.DoFn):
     def process(self,element):
-        unlab_vectors = unlab_cvect.fit([element['text']])
-        unlab_matrix = unlab_vectors.transform([element['text']])
-        df_vectors = pd.DataFrame(unlab_matrix.todense())
-        df_vectors = df_vectors.apply(lambda row:tally_counts_doc(row),axis=1,result_type='expand')
+        master = []
+        tokens = tokenize(element['text'])
 
-        yield df_vectors[['hate_score','hate_hits']].to_dict()
+        for n in range(2,4):
+            current = ngrams(tokens,n)
+            master += current
+          
 
-  
+        master += tokens
 
+        hate_score = 0
+        hate_hits = {}
+
+        for gram in master:
+            if gram in dict_hateweights.keys():
+                if gram in hate_hits:
+                    hate_hits[gram] += 1 
+                else:
+                    hate_hits[gram] = 1
+                hate_score += dict_hateweights[gram]
+        element['hate_score'] = hate_score
+        element['hate_hits'] = str(hate_hits)
+
+        if hate_score > 0:
+            yield element
+        else:
+            return
+
+schema = pyarrow.schema([
+    ('conv_id', pyarrow.string()),
+    ('timestamp',pyarrow.int64()),
+    ('source',pyarrow.string()),
+    ('text',pyarrow.string()),
+    ('hate_score',pyarrow.int64()),
+    ('hate_hits',pyarrow.string())
+    ])
 
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", dest="input", default = "data/samp_utterances.parquet")
-    parser.add_argument("--output", dest="output", default = "data/")
+    parser.add_argument("--output", dest="output", default = "data/samp_out")
     app_args, pipeline_args = parser. parse_known_args()
 
 
@@ -73,11 +92,13 @@ def run():
         
         data = p | beam.io.parquetio.ReadFromParquet(app_args.input)
         output = data | beam.ParDo(WordExtractingDoFn())
-        output | beam.io.textio.WriteToText('data/samp_output.csv')
+        output | beam.io.parquetio.WriteToParquet(app_args.output,schema=schema)
 
 
 
 run()
+
+
 
 from datetime import datetime
 print(datetime.now())
